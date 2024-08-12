3d56da86040e8b56421afc5404d9f68034dca1fc
Lata Thakuri <latathakurathi@gmail.com>
2024-08-12 19:22:36 +0545

    cleanup

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,6 @@
 const Profile = () => {
     return (
-        <div className="h-full w-full shadow bg-white rounded-0.75 dark:bg-gray-800">
-            Profile
-        </div>
+        <div className="h-full w-full shadow bg-white rounded-0.75 dark:bg-gray-800"></div>
     );
 };
 
--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -2,9 +2,7 @@ const Settings = () => {
     return (
         <div
             className="h-full w-full shadow bg-white rounded-0.75 
-        dark:bg-gray-800">
-            Settings
-        </div>
+        dark:bg-gray-800"></div>
     );
 };
 
